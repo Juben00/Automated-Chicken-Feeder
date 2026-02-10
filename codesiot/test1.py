a72655deb7df1037ac4cb8f11a254d5ce2bb2b7d
Lloydy2 <geraldll2017@gmail.com>
2026-02-10 16:10:38 +0800

    ghjg

--- codesiot/test1.py
+++ codesiot/test1.py
@@ -1,15 +1,21 @@
 from hx711 import HX711
 import time
 
-hx = HX711(5, 6)  # DT, SCK
+hx = HX711(5, 6)
 
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(1)
+hx.reset()
 hx.tare()
-print("HX711 ready – raw readings")
+
+print("HX711 ready")
 
 try:
     while True:
-        raw = hx.read()
-        print("Raw:", raw)
+        value = hx.get_weight(5)
+        print("Raw-ish value:", value)
+        hx.power_down()
+        hx.power_up()
         time.sleep(0.5)
 
 except KeyboardInterrupt:
